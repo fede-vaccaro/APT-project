d4cf1744073ca9a72c3f23690fb328aced7b8775
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-14 09:56:32 +0200

    feat: added exception classes for illegal Item argument

---
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/exceptions/IllegalName.java
@@ -0,0 +1,25 @@
+package com.unifiprojects.app.appichetto.exceptions;
+
+public class IllegalName extends IllegalArgumentException{
+
+	public IllegalName() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalName(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalName(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalName(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/exceptions/IllegalUsers.java
@@ -0,0 +1,25 @@
+package com.unifiprojects.app.appichetto.exceptions;
+
+public class IllegalUsers extends IllegalArgumentException{
+
+	public IllegalUsers() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalUsers(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalUsers(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalUsers(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
