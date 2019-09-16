47def3664d3943cada77beb0b0eac65ca4504a42
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-16 19:23:53 +0200

    chore: changed name description -> quantity

--- com.unifiprojects.app.appichetto/src/test/java/com/unifiprojects/app/appichetto/modelsTests/ItemTest.java
+++ com.unifiprojects.app.appichetto/src/test/java/com/unifiprojects/app/appichetto/modelsTests/ItemTest.java
@@ -2,20 +2,14 @@ package com.unifiprojects.app.appichetto.modelsTests;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.mockito.Mockito.verify;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
 import com.unifiprojects.app.appichetto.exceptions.IllegalName;
 import com.unifiprojects.app.appichetto.exceptions.IllegalPrice;
+import com.unifiprojects.app.appichetto.exceptions.IllegalQuantity;
 import com.unifiprojects.app.appichetto.exceptions.IllegalUsers;
 import com.unifiprojects.app.appichetto.models.Item;
 import com.unifiprojects.app.appichetto.models.User;
@@ -26,11 +20,11 @@ public class ItemTest {
 	public void testNewItemWithNullName() {
 		String name = null;
 		String price = null;
-		String description = null;
+		String quantity = null;
 		List<User> users = null;
 		
 		try {
-			new Item( name, price, description, users);
+			new Item( name, price, quantity, users);
 			fail("Name is null");
 		}catch(IllegalName e) {
 			assertEquals(e.getMessage(), "Name is empty");
@@ -42,11 +36,11 @@ public class ItemTest {
 	public void testNewItemWithEmptyName() {
 		String name = "";
 		String price = null;
-		String description = null;
+		String quantity = null;
 		List<User> users = null;
 		
 		try {
-			new Item( name, price, description, users);
+			new Item( name, price, quantity, users);
 			fail("Name is empty");
 		}catch(IllegalName e) {
 			assertEquals(e.getMessage(), "Name is empty");
@@ -57,11 +51,11 @@ public class ItemTest {
 	public void testNewItemWithNullPrice() {
 		String name = "Item";
 		String price = null;
-		String description = null;
+		String quantity = null;
 		List<User> users = null;
 		
 		try {
-			new Item( name, price, description, users);
+			new Item( name, price, quantity, users);
 			fail("Price is null");
 		}catch(IllegalPrice e) {
 			assertEquals(e.getMessage(), "Price is empty");
@@ -69,15 +63,15 @@ public class ItemTest {
 	}
 
 	@Test
-	public void testNewItemWithStringPrice() {
+	public void testNewItemWithNotDoublePrice() {
 		String name = "Item";
 		String price = "aaa";
-		String description = null;
+		String quantity = null;
 		List<User> users = null;
 		
 		try {
-			new Item( name, price, description, users);
-			fail("Price is null");
+			new Item( name, price, quantity, users);
+			fail("Price is not double");
 		}catch(IllegalPrice e) {
 			assertEquals(e.getMessage(), "Price is not double");
 		}
@@ -87,14 +81,59 @@ public class ItemTest {
 	public void testNewItemWithZeroPrice() {
 		String name = "Item";
 		String price = "0";
-		String description = null;
+		String quantity = null;
 		List<User> users = null;
 		
 		try {
-			new Item( name, price, description, users);
-			fail("Price is null");
+			new Item( name, price, quantity, users);
+			fail("Price is zero");
 		}catch(IllegalPrice e) {
 			assertEquals(e.getMessage(), "Price is zero");
+		}
+	}
+
+	@Test
+	public void testNewItemWithNullQuantity() {
+		String name = "Item";
+		String price = "1";
+		String quantity = null;
+		List<User> users = null;
+		
+		try {
+			new Item( name, price, quantity, users);
+			fail("Quantity is null");
+		}catch(IllegalQuantity e) {
+			assertEquals(e.getMessage(), "Quantity is not int");
+		}
+	}
+	
+	@Test
+	public void testNewItemWithNotIntegerQuantity() {
+		String name = "Item";
+		String price = "1";
+		String quantity = "aaa"; 
+		List<User> users = null;
+		
+		try {
+			new Item( name, price, quantity, users);
+			fail("Quantity is not int");
+		}catch(IllegalQuantity e) {
+			assertEquals(e.getMessage(), "Quantity is not int");
+		}
+	}
+
+	@Test
+	public void testNewItemWithZeroQuantity() {
+		String name = "Item";
+		String price = "1";
+		String quantity = "0"; 
+		List<User> users = null;
+		
+		try {
+			new Item( name, price, quantity, users);
+			fail("Quantity is zero");
+		}catch(IllegalQuantity e) {
+			assertEquals(e.getMessage(), "Quantity is zero");
 		}
 	}
 
@@ -102,11 +141,11 @@ public class ItemTest {
 	public void testNewItemWithNullUser() {
 		String name = "Item";
 		String price = "2";
-		String description = null;
+		String quantity = "1";
 		List<User> users = null;
 		
 		try {
-			new Item( name, price, description, users);
+			new Item( name, price, quantity, users);
 			fail("Users is null");
 		}catch(IllegalUsers e) {
 			assertEquals(e.getMessage(), "Users list is empty");
@@ -117,11 +156,11 @@ public class ItemTest {
 	public void testNewItemWithEmptyUser() {
 		String name = "Item";
 		String price = "2";
-		String description = null;
+		String quantity = "1";
 		List<User> users = new ArrayList<User>();
 		
 		try {
-			new Item( name, price, description, users);
+			new Item( name, price, quantity, users);
 			fail("Users is null");
 		}catch(IllegalUsers e) {
 			assertEquals(e.getMessage(), "Users list is empty");
@@ -132,10 +171,10 @@ public class ItemTest {
 	public void testNewItem() {
 		String name = "Item";
 		String price = "2";
-		String description = null;
+		String quantity = "1";
 		List<User> users = new ArrayList<User>(Arrays.asList(new User()));
 		
-		Item item =	new Item( name, price, description, users);
+		Item item =	new Item( name, price, quantity, users);
 
 	}
 
