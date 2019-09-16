95b9346fc35211b532bcab4aedd4bb7d2f781294
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-16 19:23:00 +0200

    chore: added quantity attribute

--- com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/models/Item.java
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/models/Item.java
@@ -1,45 +1,82 @@
 package com.unifiprojects.app.appichetto.models;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 import com.unifiprojects.app.appichetto.exceptions.IllegalName;
 import com.unifiprojects.app.appichetto.exceptions.IllegalPrice;
+import com.unifiprojects.app.appichetto.exceptions.IllegalQuantity;
 import com.unifiprojects.app.appichetto.exceptions.IllegalUsers;
 
 public class Item {
 	private Long id;
 	private String name;
-	private String description;
+	private Integer quantity;
 	private Double price;
 	private List<User> users;
 
-	public Item(String name, String stringPrice, String description, List<User> users) {
-		
+	public Item(String name, String stringPrice, String stringQuantity, List<User> users) {
+
 		Double price = 0.0;
-		
+		Integer quantity = 0;
+
 		if (Objects.isNull(name) || name.isEmpty())
 			throw new IllegalName("Name is empty");
 
 		try {
 			price = Double.parseDouble(stringPrice);
-		}catch(NumberFormatException e) {
+		} catch (NumberFormatException e) {
 			throw new IllegalPrice("Price is not double");
 		} catch (NullPointerException e) {
 			throw new IllegalPrice("Price is empty");
-		}		
-		
-		if ( price == 0.0 )
+		}
+
+		if (price == 0.0)
 			throw new IllegalPrice("Price is zero");
+
+		try {
+			quantity = Integer.parseInt(stringQuantity);
+		} catch (NullPointerException e) {
+			throw new IllegalQuantity("Quantity is empty");
+
+		} catch (NumberFormatException e) {
+			throw new IllegalQuantity("Quantity is not int");
+		}
 		
-		if ( Objects.isNull(users) || users.isEmpty())
+		if (quantity == 0.0)
+			throw new IllegalQuantity("Quantity is zero");
+
+		if (Objects.isNull(users) || users.isEmpty())
 			throw new IllegalUsers("Users list is empty");
-		
+
 		this.name = name;
 		this.price = price;
 		this.users = users;
-		this.description = description;	
-		
+		this.quantity = quantity;
+
+	}
+	
+
+	public String toString() {
+		return this.name+" x"+this.quantity;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	
 }
