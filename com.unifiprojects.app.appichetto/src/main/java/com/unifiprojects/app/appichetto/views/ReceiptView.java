bcbcd487f1968a347f64abe430594be087d5121e
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-16 19:31:22 +0200

    chore: added itemAdded, itemRemoved and itemUpdated methods

--- com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/views/ReceiptView.java
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/views/ReceiptView.java
@@ -3,14 +3,23 @@ package com.unifiprojects.app.appichetto.views;
 import java.util.List;
 
 import com.unifiprojects.app.appichetto.models.Item;
+import com.unifiprojects.app.appichetto.models.User;
 
 public interface ReceiptView {
-	public void showCurrentItemList(List<Item> items);
+	public void showCurrentItemsList(List<Item> items);
+
+	public void showCurrentUsers(List<User> users);
 
 	public void showError(String string);
 
 	public void showDoneMsg(String string);
 
 	public void showSelectedItem(Item item);
+
+	public void itemAdded(Item item);
+	
+	public void itemDeleted(Item item);
+
+	public void itemUpdated(int index, Item item);
 
 }
