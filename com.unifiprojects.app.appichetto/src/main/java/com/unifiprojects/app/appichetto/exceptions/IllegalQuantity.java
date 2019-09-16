ec5029f11d98417779a9b1e76f5bc3424fbbbb7b
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-16 19:21:23 +0200

    feat: added IllegalQuantity exception class

---
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/exceptions/IllegalQuantity.java
@@ -0,0 +1,25 @@
+package com.unifiprojects.app.appichetto.exceptions;
+
+public class IllegalQuantity extends IllegalArgumentException {
+
+	public IllegalQuantity() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalQuantity(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalQuantity(String s) {
+		super(s);
+		// TODO Auto-generated constructor stub
+	}
+
+	public IllegalQuantity(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
