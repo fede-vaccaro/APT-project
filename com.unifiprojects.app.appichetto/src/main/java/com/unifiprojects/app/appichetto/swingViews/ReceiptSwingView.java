3599f1732d46aba5821dc159812dc49060feb90e
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-16 19:26:15 +0200

    feat: added save, update and delete functionality and related tests

--- com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/swingViews/ReceiptSwingView.java
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/swingViews/ReceiptSwingView.java
@@ -1,34 +1,82 @@
 package com.unifiprojects.app.appichetto.swingViews;
 
+import java.awt.Checkbox;
+import java.awt.CheckboxGroup;
+import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import com.jgoodies.forms.layout.FormLayout;
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.RowSpec;
+import com.unifiprojects.app.appichetto.controls.ReceiptController;
 import com.unifiprojects.app.appichetto.models.Item;
+import com.unifiprojects.app.appichetto.models.User;
+import com.unifiprojects.app.appichetto.views.ReceiptView;
 import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+import javax.swing.ButtonGroup;
 import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import javax.swing.JList;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.event.ListSelectionEvent;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-public class ReceiptSwingView extends JFrame {
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.event.ListSelectionListener;
+
+import org.assertj.swing.edt.GuiActionRunner;
+
+import javax.swing.event.ListSelectionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class ReceiptSwingView extends JFrame implements ReceiptView {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private JTextField txtPrice;
 	private JTextField txtName;
 	private JTextField txtQuantity;
 	private JLabel lblQuantity;
-	private JScrollPane scrollPane;
+	private JScrollPane usersScrollPane;
+	private JScrollPane itemsScrollPane;
 	private JButton btnSave;
 	private JButton btnDelete;
 	private JList<Item> itemsList;
 	private DefaultListModel<Item> listItemModel;
+	private JLabel errorMsgLabel;
+
+	private ReceiptController receiptController;
+	private JList<User> usersList;
+
+	public void setUsersList(JList<User> usersList) {
+		this.usersList = usersList;
+	}
+
+	private DefaultListModel<User> listUsersModel;
+	private JButton btnNewButton;
+	private JButton btnUpdate;
+
+	public void setReceiptController(ReceiptController receiptController) {
+		this.receiptController = receiptController;
+		receiptController.getUsers().stream().forEach(listUsersModel::addElement);
+	}
 
 	/**
 	 * Launch the application.
@@ -50,10 +98,62 @@ public class ReceiptSwingView extends JFrame {
 		return listItemModel;
 	}
 
+	public DefaultListModel<User> getListUsersModel() {
+		return listUsersModel;
+	}
+
+	public JList<User> getUsersList() {
+		return usersList;
+	}
+
+	@Override
+	public void showCurrentItemsList(List<Item> items) {
+		items.stream().forEach(listItemModel::addElement);
+	}
+
+	@Override
+	public void showError(String string) {
+		errorMsgLabel.setText(string);
+	}
+
+	@Override
+	public void showDoneMsg(String string) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showSelectedItem(Item item) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void showCurrentUsers(List<User> users) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void itemAdded(Item item) {
+		listItemModel.addElement(item);
+		resetErrorLabel();
+	}
+
+	@Override
+	public void itemDeleted(Item item) {
+		listItemModel.removeElement(item);
+		resetErrorLabel();
+	}
+
+	private void resetErrorLabel() {
+		errorMsgLabel.setText(" ");
+	}
+
 	/**
 	 * Create the frame.
 	 */
 	public ReceiptSwingView() {
+
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new FormLayout(
@@ -72,7 +172,9 @@ public class ReceiptSwingView extends JFrame {
 						FormSpecs.RELATED_GAP_ROWSPEC, RowSpec.decode("default:grow"), FormSpecs.RELATED_GAP_ROWSPEC,
 						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
 						FormSpecs.RELATED_GAP_ROWSPEC, RowSpec.decode("default:grow"), FormSpecs.DEFAULT_ROWSPEC,
-						FormSpecs.DEFAULT_ROWSPEC, }));
+						FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+						FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+						FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, }));
 
 		JLabel lblName = new JLabel("Name");
 		getContentPane().add(lblName, "24, 2");
@@ -84,16 +186,6 @@ public class ReceiptSwingView extends JFrame {
 
 		JLabel lblPrice = new JLabel("Price");
 		getContentPane().add(lblPrice, "24, 6");
-
-		listItemModel = new DefaultListModel<>();
-		itemsList = new JList<>(listItemModel);
-		itemsList.setName("itemsList");
-		itemsList.addListSelectionListener(new ListSelectionListener() {
-			public void valueChanged(ListSelectionEvent arg0) {
-				btnSave.setEnabled(itemsList.getSelectedIndex() != -1);
-			}
-		});
-		getContentPane().add(itemsList, "3, 8, 13, 7, fill, fill");
 
 		txtPrice = new JTextField();
 		txtPrice.setName("priceBox");
@@ -108,28 +200,109 @@ public class ReceiptSwingView extends JFrame {
 		getContentPane().add(txtQuantity, "24, 12, fill, default");
 		txtQuantity.setColumns(10);
 
+		usersScrollPane = new JScrollPane();
+		listUsersModel = new DefaultListModel<>();
+		usersList = new JList<>(listUsersModel);
+		usersList.setName("usersList");
+		usersList.setSelectionModel(new DefaultListSelectionModel() {
+			private int i0 = -1;
+			private int i1 = -1;
+
+			public void setSelectionInterval(int index0, int index1) {
+				if (i0 == index0 && i1 == index1) {
+					if (getValueIsAdjusting()) {
+						setValueIsAdjusting(false);
+						setSelection(index0, index1);
+					}
+				} else {
+					i0 = index0;
+					i1 = index1;
+					setValueIsAdjusting(false);
+					setSelection(index0, index1);
+				}
+			}
+
+			private void setSelection(int index0, int index1) {
+				if (super.isSelectedIndex(index0)) {
+					super.removeSelectionInterval(index0, index1);
+				} else {
+					super.addSelectionInterval(index0, index1);
+				}
+			}
+		});
+		usersList.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
+
+		usersList.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent arg0) {
+				btnSave.setEnabled(!txtName.getText().trim().isEmpty() && !txtPrice.getText().trim().isEmpty()
+						&& !txtQuantity.getText().trim().isEmpty() && !usersList.isSelectionEmpty());
+			}
+		});
+		usersScrollPane.setViewportView(usersList);
+		getContentPane().add(usersScrollPane, "24, 14, fill, fill");
+		itemsScrollPane = new JScrollPane();
+
+		listItemModel = new DefaultListModel<>();
+		itemsList = new JList<>(listItemModel);
+		itemsList.setName("itemsList");
+		itemsScrollPane.setViewportView(itemsList);
+		itemsList.addListSelectionListener(new ListSelectionListener() {
+			public void valueChanged(ListSelectionEvent arg0) {
+				Item item = itemsList.getSelectedValue();
+				txtName.setText(item.getName());
+				txtPrice.setText(item.getPrice().toString());
+				txtQuantity.setText(item.getQuantity().toString());
+				int[] indeces = item.getUsers().stream().mapToInt(user -> listUsersModel.indexOf(user)).toArray();
+				usersList.setSelectedIndices(indeces);
+				btnSave.setEnabled(itemsList.getSelectedIndex() != -1);
+				btnUpdate.setEnabled(itemsList.getSelectedIndex() != -1);
+				btnDelete.setEnabled(itemsList.getSelectedIndex() != -1);
+			}
+		});
+		getContentPane().add(itemsScrollPane, "3, 8, 13, 7, fill, fill");
+
+		errorMsgLabel = new JLabel("");
+		errorMsgLabel.setName("errorMsgLabel");
+		errorMsgLabel.setForeground(Color.RED);
+		getContentPane().add(errorMsgLabel, "8, 15, 7, 1");
+
+		btnSave = new JButton("Save");
+		btnSave.addActionListener(e -> receiptController.addItem(txtName.getText(), txtPrice.getText(),
+				txtQuantity.getText(), usersList.getSelectedValuesList()));
+		btnSave.setEnabled(false);
+		getContentPane().add(btnSave, "24, 17");
+
+		btnDelete = new JButton("Delete");
+		btnDelete.addActionListener(e -> receiptController.deleteItem(itemsList.getSelectedValue()));
+
+		btnUpdate = new JButton("Update");
+		btnUpdate.addActionListener(
+				e -> receiptController.updateItem(txtName.getText(), txtPrice.getText(), txtQuantity.getText(),
+						usersList.getSelectedValuesList(), listUsersModel.indexOf(itemsList.getSelectedValue())));
+		btnUpdate.setEnabled(false);
+		getContentPane().add(btnUpdate, "24, 19");
+
+		btnDelete.setEnabled(false);
+		getContentPane().add(btnDelete, "24, 21");
+
 		KeyAdapter btnSaveEnabled = new KeyAdapter() {
 
 			@Override
 			public void keyReleased(KeyEvent arg0) {
 				btnSave.setEnabled(!txtName.getText().trim().isEmpty() && !txtPrice.getText().trim().isEmpty()
-						&& !txtQuantity.getText().trim().isEmpty());
+						&& !txtQuantity.getText().trim().isEmpty() && !usersList.isSelectionEmpty());
 			}
 		};
 		txtName.addKeyListener(btnSaveEnabled);
 		txtPrice.addKeyListener(btnSaveEnabled);
 		txtQuantity.addKeyListener(btnSaveEnabled);
 
-		scrollPane = new JScrollPane();
-		getContentPane().add(scrollPane, "24, 14, fill, fill");
+		this.pack();
 
-		btnSave = new JButton("Save");
-		btnSave.setEnabled(false);
-		getContentPane().add(btnSave, "24, 15");
-
-		btnDelete = new JButton("Delete");
-		btnDelete.setEnabled(false);
-		getContentPane().add(btnDelete, "24, 16");
+//		User user = new User("pippo", "psw");
+//		User user1 = new User("pluto", "psw");
+//		GuiActionRunner.execute(() -> this.getListUsersModel().addElement(user));
+//		GuiActionRunner.execute(() -> this.getListUsersModel().addElement(user1));
+//		usersList.setSelectedIndices(new int[] {0,1});
 	}
-
 }
--- com.unifiprojects.app.appichetto/src/test/java/com/unifiprojects/app/appichetto/swingViews/ReceiptSwingViewTest.java
+++ com.unifiprojects.app.appichetto/src/test/java/com/unifiprojects/app/appichetto/swingViews/ReceiptSwingViewTest.java
@@ -1,10 +1,14 @@
 package com.unifiprojects.app.appichetto.swingViews;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
 
 import org.assertj.swing.annotation.GUITest;
 import org.assertj.swing.core.matcher.JButtonMatcher;
@@ -17,7 +21,10 @@ import org.assertj.swing.junit.runner.GUITestRunner;
 import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import com.unifiprojects.app.appichetto.controls.ReceiptController;
 import com.unifiprojects.app.appichetto.models.Item;
 import com.unifiprojects.app.appichetto.models.User;
 
@@ -27,9 +34,27 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 	private FrameFixture window;
 	private ReceiptSwingView receiptSwingView;
 
+	private User addUserToList(String name, String psw) {
+		User user = new User(name, psw);
+		GuiActionRunner.execute(() -> receiptSwingView.getListUsersModel().addElement(user));
+		return user;
+	}
+
+	private Item addItemToList(String name, String stringPrice, String stringQuantity, List<User> users) {
+		Item item = new Item(name, stringPrice, stringQuantity, users);
+		GuiActionRunner.execute(() -> receiptSwingView.getListItemModel().addElement(item));
+		return item;
+
+	}
+
+	@Mock
+	private ReceiptController receiptController;
+
 	protected void onSetUp() {
+		MockitoAnnotations.initMocks(this);
 		GuiActionRunner.execute(() -> {
 			receiptSwingView = new ReceiptSwingView();
+			receiptSwingView.setReceiptController(receiptController);
 			return receiptSwingView;
 		});
 		window = new FrameFixture(robot(), receiptSwingView);
@@ -48,19 +73,41 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		window.label(JLabelMatcher.withText("Quantity"));
 		window.textBox("quantityBox").requireEnabled();
 
-		// TODO test scroll panel is present
+		// TODO test both scroll panels are present
 
 		window.button(JButtonMatcher.withText("Save")).requireDisabled();
+		window.button(JButtonMatcher.withText("Update")).requireDisabled();
+		window.button(JButtonMatcher.withText("Delete")).requireDisabled();
+		window.label("errorMsgLabel").requireText("");
+	}
+
+	@Test
+	@GUITest
+	public void testWhenFormIsNonEmptyThenAddButtonShouldBeEnabledCompilingFirstItemInfoThenUsersList() {
+		window.textBox("nameBox").enterText("Sugo");
+		window.textBox("priceBox").enterText("2.2");
+		window.textBox("quantityBox").enterText("2");
+		addUserToList("Pippo", "psw");
+
+		window.list("usersList").selectItem(0);
+
+		window.button(JButtonMatcher.withText("Save")).requireEnabled();
+		window.button(JButtonMatcher.withText("Update")).requireDisabled();
 		window.button(JButtonMatcher.withText("Delete")).requireDisabled();
 	}
 
 	@Test
 	@GUITest
-	public void testWhenFontsAreNonEmptyThenAddButtonShouldBeEnabled() {
-		window.textBox("nameBox").enterText("Pippo");
+	public void testWhenFormIsNonEmptyThenAddButtonShouldBeEnabledCompilingFirstUsersListThenItemInfo() {
+		addUserToList("Pippo", "psw");
+		window.list("usersList").selectItem(0);
+		window.textBox("nameBox").enterText("Sugo");
 		window.textBox("priceBox").enterText("2.2");
 		window.textBox("quantityBox").enterText("2");
+
 		window.button(JButtonMatcher.withText("Save")).requireEnabled();
+		window.button(JButtonMatcher.withText("Update")).requireDisabled();
+		window.button(JButtonMatcher.withText("Delete")).requireDisabled();
 	}
 
 	@Test
@@ -70,7 +117,9 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		JTextComponentFixture priceBox = window.textBox("priceBox");
 		JTextComponentFixture quantityBox = window.textBox("quantityBox");
 
-		nameBox.enterText("Pippo");
+		addUserToList("Pippo", "psw");
+		window.list("usersList").selectItem(0);
+		nameBox.enterText("Sugo");
 		priceBox.enterText("  ");
 		quantityBox.enterText("2");
 		window.button(JButtonMatcher.withText("Save")).requireDisabled();
@@ -87,16 +136,122 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 	}
 
 	@Test
-	public void testSaveButtonShouldBeEnabledOnlyWhenAItemIsSelected() {
+	public void testSaveUpdateAndDeleteButtonsShouldBeEnabledOnlyWhenAnItemIsSelected() {
 		List<User> users = new ArrayList<User>(Arrays.asList(new User()));
 
-		GuiActionRunner
-				.execute(() -> receiptSwingView.getListItemModel().addElement(new Item("Pippo", "2.2", "2", users)));
+		addItemToList("Sugo", "2.2", "2", users);
 		window.list("itemsList").selectItem(0);
 		JButtonFixture saveButton = window.button(JButtonMatcher.withText("Save"));
+		JButtonFixture updateButton = window.button(JButtonMatcher.withText("Update"));
+		JButtonFixture deleteButton = window.button(JButtonMatcher.withText("Delete"));
 		saveButton.requireEnabled();
-		window.list("itemsList").clearSelection();
-		saveButton.requireDisabled();
+		updateButton.requireEnabled();		
+		deleteButton.requireEnabled();
 	}
+
+	@Test
+	public void testShowCurrentItemsListShouldAddItemDescriptionsToTheList() {
+		List<User> users = new ArrayList<User>(Arrays.asList(new User()));
+
+		Item item1 = new Item("Sugo", "1", "1", users);
+		Item item2 = new Item("Pasta", "2", "1", users);
+		GuiActionRunner.execute(() -> receiptSwingView.showCurrentItemsList(Arrays.asList(item1, item2)));
+		String[] listContents = window.list("itemsList").contents();
+		assertThat(listContents).containsExactly(item1.toString(), item2.toString());
+	}
+
+	@Test
+	public void testShowErrorMessage() {
+		GuiActionRunner.execute(() -> receiptSwingView.showError("Item name is null"));
+		window.label("errorMsgLabel").requireText("Item name is null");
+	}
+
+	@Test
+	public void testItemAddedShouldAddTheItemToTheListAndResetTheErrorLabel() {
+		List<User> users = new ArrayList<User>(Arrays.asList(new User()));
+		Item item = new Item("Sugo", "1", "1", users);
+
+		GuiActionRunner.execute(() -> receiptSwingView.itemAdded(item));
+		String[] listContents = window.list("itemsList").contents();
+		assertThat(listContents).containsExactly(item.toString());
+		window.label("errorMsgLabel").requireText(" ");
+	}
+
+	@Test
+	public void testItemDeletedShouldDeleteTheItemFromTheListAndResetTheErrorLabel() {
+		List<User> users = new ArrayList<User>(Arrays.asList(new User()));
+		Item item1 = new Item("Sugo", "1", "1", users);
+		Item item2 = new Item("Pasta", "1", "1", users);
+
+		GuiActionRunner.execute(() -> {
+			receiptSwingView.getListItemModel().addElement(item1);
+			receiptSwingView.getListItemModel().addElement(item2);
+		});
+
+		GuiActionRunner.execute(() -> receiptSwingView.itemDeleted(item1));
+
+		String[] listContents = window.list("itemsList").contents();
+		assertThat(listContents).containsExactly(item2.toString());
+		window.label("errorMsgLabel").requireText(" ");
+	}
+	
+	@Test
+	@GUITest
+	public void testWhenAnItemIsSelectedThenFormIsCompiledWithItsArguments() {
+		User user = addUserToList("Pippo", "psw");
+		User user1 = addUserToList("Pluto", "psw");
+		addItemToList("Sugo", "2.2", "2", Arrays.asList(user, user1));
+		JTextComponentFixture nameBox = window.textBox("nameBox");
+		JTextComponentFixture priceBox = window.textBox("priceBox");
+		JTextComponentFixture quantityBox = window.textBox("quantityBox");
+
+		window.list("itemsList").selectItem(0);
+		nameBox.requireText("Sugo");
+		priceBox.requireText("2.2");
+		quantityBox.requireText("2");
+		assertTrue(receiptSwingView.getUsersList().getSelectedValuesList().containsAll(Arrays.asList(user, user1)));
+	}
+	
+	@Test
+	public void testAddButtonShouldDelegateToReceiptControllerNewItem() {
+		User user1 = addUserToList("Pippo", "psw");
+		User user2 = addUserToList("Pluto", "psw");
+
+		window.textBox("nameBox").enterText("Sugo");
+		window.textBox("priceBox").enterText("2");
+		window.textBox("quantityBox").enterText("2");
+		window.list("usersList").selectItem(0);
+		window.list("usersList").selectItem(1);
+
+		window.button(JButtonMatcher.withText("Save")).click();
+		verify(receiptController).addItem("Sugo", "2", "2", Arrays.asList(user1, user2));
+	}
+
+	@Test
+	public void testUpdateButtonShouldDelegateToReceiptControllerUpdateItem() {
+		User user1 = addUserToList("Pippo", "psw");
+		User user2 = addUserToList("Pluto", "psw");
+		
+		Item item = addItemToList("Sugo", "1", "1", Arrays.asList(user1,user2));
+		Item item1 = addItemToList("Pasta", "1", "1", Arrays.asList(user1,user2));
+
+		window.list("itemsList").selectItem(1);
+		
+		window.button(JButtonMatcher.withText("Update")).click();
+		verify(receiptController).updateItem("Pasta", "1.0", "1", Arrays.asList(user1, user2), receiptSwingView.getListUsersModel().indexOf(item));
+	}
+
+	@Test
+	public void testDeleteButtonShouldDelegateToReceiptControllerDeleteItem() {
+		List<User> users = new ArrayList<User>(Arrays.asList(new User()));
+		Item item = addItemToList("Sugo", "1", "1", users);
+
+		window.list("itemsList").selectItem(0);
+		
+		window.button(JButtonMatcher.withText("Delete")).click();
+		verify(receiptController).deleteItem(item);
+		
+	}
+	
 
 }
