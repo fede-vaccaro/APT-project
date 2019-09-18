4ba40f26ebf65899b6f4e66889ab150422ace558
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-18 18:48:56 +0200

    feat: added tests for save and update buttons

--- com.unifiprojects.app.appichetto/src/test/java/com/unifiprojects/app/appichetto/swingViews/ReceiptSwingViewTest.java
+++ com.unifiprojects.app.appichetto/src/test/java/com/unifiprojects/app/appichetto/swingViews/ReceiptSwingViewTest.java
@@ -22,11 +22,9 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
-import com.unifiprojects.app.appichetto.controls.ReceiptController;
+import com.unifiprojects.app.appichetto.controllers.ReceiptController;
 import com.unifiprojects.app.appichetto.models.Item;
 import com.unifiprojects.app.appichetto.models.User;
-
-
 
 @RunWith(GUITestRunner.class)
 public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
@@ -94,6 +92,9 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		window.button(JButtonMatcher.withText("Save")).requireEnabled();
 		window.button(JButtonMatcher.withText("Update")).requireDisabled();
 		window.button(JButtonMatcher.withText("Delete")).requireDisabled();
+		window.button(JButtonMatcher.withText("Save Receipt")).requireDisabled();
+		window.button(JButtonMatcher.withText("Home")).requireEnabled();
+		window.button(JButtonMatcher.withText("Exit")).requireEnabled();
 	}
 
 	@Test
@@ -112,7 +113,7 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 
 	@Test
 	@GUITest
-	public void testWhenOneOrMoreArgumentAreBlanckThenSaveButtonIsDisabled() {
+	public void testWhenOneOrMoreArgumentAreBlanckThenSaveAndUpdateButtonsAreDisabled() {
 		JTextComponentFixture nameBox = window.textBox("nameBox");
 		JTextComponentFixture priceBox = window.textBox("priceBox");
 		JTextComponentFixture quantityBox = window.textBox("quantityBox");
@@ -123,6 +124,7 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		priceBox.enterText("  ");
 		quantityBox.enterText("2");
 		window.button(JButtonMatcher.withText("Save")).requireDisabled();
+		window.button(JButtonMatcher.withText("Update")).requireDisabled();
 
 		nameBox.setText("");
 		priceBox.setText("");
@@ -132,6 +134,7 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		priceBox.setText("2.2");
 		quantityBox.setText("  ");
 		window.button(JButtonMatcher.withText("Save")).requireDisabled();
+		window.button(JButtonMatcher.withText("Update")).requireDisabled();
 
 	}
 
@@ -145,7 +148,7 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		JButtonFixture updateButton = window.button(JButtonMatcher.withText("Update"));
 		JButtonFixture deleteButton = window.button(JButtonMatcher.withText("Delete"));
 		saveButton.requireEnabled();
-		updateButton.requireEnabled();		
+		updateButton.requireEnabled();
 		deleteButton.requireEnabled();
 	}
 
@@ -158,6 +161,16 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		GuiActionRunner.execute(() -> receiptSwingView.showCurrentItemsList(Arrays.asList(item1, item2)));
 		String[] listContents = window.list("itemsList").contents();
 		assertThat(listContents).containsExactly(item1.toString(), item2.toString());
+	}
+
+	@Test
+	public void testShowCurrentUsersListShouldAddUserDescriptionsToTheList() {
+		User user1 = new User("Pippo", "psw");
+		User user2 = new User("Pluto", "psw");
+
+		GuiActionRunner.execute(() -> receiptSwingView.showCurrentUsers(Arrays.asList(user1, user2)));
+		String[] listContents = window.list("usersList").contents();
+		assertThat(listContents).containsExactly(user1.toString(), user2.toString());
 	}
 
 	@Test
@@ -194,10 +207,13 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		assertThat(listContents).containsExactly(item2.toString());
 		window.label("errorMsgLabel").requireText(" ");
 	}
-	
+
 	@Test
 	@GUITest
 	public void testWhenAnItemIsSelectedThenFormIsCompiledWithItsArguments() {
+		JButtonFixture saveButton = window.button(JButtonMatcher.withText("Save"));
+		JButtonFixture updateButton = window.button(JButtonMatcher.withText("Update"));
+		JButtonFixture deleteButton = window.button(JButtonMatcher.withText("Delete"));
 		User user = addUserToList("Pippo", "psw");
 		User user1 = addUserToList("Pluto", "psw");
 		addItemToList("Sugo", "2.2", "2", Arrays.asList(user, user1));
@@ -210,48 +226,86 @@ public class ReceiptSwingViewTest extends AssertJSwingJUnitTestCase {
 		priceBox.requireText("2.2");
 		quantityBox.requireText("2");
 		assertTrue(receiptSwingView.getUsersList().getSelectedValuesList().containsAll(Arrays.asList(user, user1)));
+		saveButton.requireEnabled();
+		updateButton.requireEnabled();
+		deleteButton.requireEnabled();
+		window.list("itemsList").clearSelection();
+		saveButton.requireDisabled();
+		updateButton.requireDisabled();
+		deleteButton.requireDisabled();
 	}
-	
+
 	@Test
-	public void testAddButtonShouldDelegateToReceiptControllerNewItem() {
+	public void testAddButtonShouldDelegateToReceiptControllerNewItemAndTheFormIsClear() {
 		User user1 = addUserToList("Pippo", "psw");
 		User user2 = addUserToList("Pluto", "psw");
+		JTextComponentFixture nameBox = window.textBox("nameBox");
+		JTextComponentFixture priceBox = window.textBox("priceBox");
+		JTextComponentFixture quantityBox = window.textBox("quantityBox");
 
-		window.textBox("nameBox").enterText("Sugo");
-		window.textBox("priceBox").enterText("2");
-		window.textBox("quantityBox").enterText("2");
+		nameBox.enterText("Sugo");
+		priceBox.enterText("2.0");
+		quantityBox.enterText("2");
 		window.list("usersList").selectItem(0);
 		window.list("usersList").selectItem(1);
 
+		Item item = new Item("Sugo", "2.0", "2", Arrays.asList(user1, user2));
+
 		window.button(JButtonMatcher.withText("Save")).click();
-		verify(receiptController).addItem("Sugo", "2", "2", Arrays.asList(user1, user2));
+
+		verify(receiptController).addItem(new Item("Sugo", "2.0", "2", Arrays.asList(user1, user2)));
+		nameBox.requireText("");
+		priceBox.requireText("");
+		quantityBox.requireText("");
+		window.list("usersList").requireNoSelection();
+		window.list("itemsList").requireNoSelection();
 	}
 
 	@Test
 	public void testUpdateButtonShouldDelegateToReceiptControllerUpdateItem() {
 		User user1 = addUserToList("Pippo", "psw");
 		User user2 = addUserToList("Pluto", "psw");
-		
-		Item item = addItemToList("Sugo", "1", "1", Arrays.asList(user1,user2));
-		Item item1 = addItemToList("Pasta", "1", "1", Arrays.asList(user1,user2));
+
+		Item item = addItemToList("Sugo", "1", "1", Arrays.asList(user1, user2));
+		Item item1 = addItemToList("Pasta", "1", "1", Arrays.asList(user1, user2));
 
 		window.list("itemsList").selectItem(1);
-		
+
 		window.button(JButtonMatcher.withText("Update")).click();
-		verify(receiptController).updateItem("Pasta", "1.0", "1", Arrays.asList(user1, user2), receiptSwingView.getListUsersModel().indexOf(item));
+		verify(receiptController).updateItem(new Item("Pasta", "1.0", "1", Arrays.asList(user1, user2)),
+				1);
+		window.list("usersList").requireNoSelection();
+		window.list("itemsList").requireNoSelection();
 	}
 
 	@Test
 	public void testDeleteButtonShouldDelegateToReceiptControllerDeleteItem() {
 		List<User> users = new ArrayList<User>(Arrays.asList(new User()));
-		Item item = addItemToList("Sugo", "1", "1", users); 
+		Item item = addItemToList("Sugo", "1", "1", users);
 
 		window.list("itemsList").selectItem(0);
-		
+
 		window.button(JButtonMatcher.withText("Delete")).click();
 		verify(receiptController).deleteItem(item);
-		
+
 	}
 	
+	@Test 
+	public void testSaveReceiptIsEnabledWithAtLeastASaveIsDone() {
+		User user1 = addUserToList("Pippo", "psw");
+		User user2 = addUserToList("Pluto", "psw");
+		JTextComponentFixture nameBox = window.textBox("nameBox");
+		JTextComponentFixture priceBox = window.textBox("priceBox");
+		JTextComponentFixture quantityBox = window.textBox("quantityBox");
+
+		nameBox.enterText("Sugo");
+		priceBox.enterText("2.0");
+		quantityBox.enterText("2");
+		window.list("usersList").selectItem(0);
+		window.list("usersList").selectItem(1);
+		window.button(JButtonMatcher.withText("Save")).click();
+
+		window.button(JButtonMatcher.withText("Save Receipt")).requireEnabled();
+	}
 
 }
