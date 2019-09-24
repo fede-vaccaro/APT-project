1811b3666502f8407da04af244084a426d8f8584
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-09-24 19:56:58 +0200

    feat: added AccountingRepository and tests

---
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/repositories/AccountingRepositoryHibernate.java
@@ -0,0 +1,37 @@
+package com.unifiprojects.app.appichetto.repositories;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+
+import com.unifiprojects.app.appichetto.models.Accounting;
+import com.unifiprojects.app.appichetto.models.User;
+
+public class AccountingRepositoryHibernate implements AccountingRepository {
+
+	private EntityManager entityManager;
+
+	public AccountingRepositoryHibernate(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+
+	@Override
+	public void saveAccounting(Accounting accounting) {
+		entityManager.getTransaction().begin();
+		entityManager.persist(accounting);
+		entityManager.getTransaction().commit();
+	}
+
+	@Override
+	public List<Accounting> getAccountingsOf(User user) {
+		return entityManager.createQuery("from Accounting where user = :user", Accounting.class)
+				.setParameter("user", user).getResultList();
+	}
+
+	@Override
+	public Accounting getById(long id) {
+		return entityManager.find(Accounting.class, id);
+	}
+
+}
