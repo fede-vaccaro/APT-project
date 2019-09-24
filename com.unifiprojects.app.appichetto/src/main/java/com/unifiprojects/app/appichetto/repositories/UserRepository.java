876cc5e26853a3baaf70f709b6214d3d51afb47a
fede-vaccaro <federico.vaccaro@live.it>
2019-09-24 19:07:07 +0200

    fix: updated UserRepository throwed exception

--- com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/repositories/UserRepository.java
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/repositories/UserRepository.java
@@ -2,10 +2,11 @@ package com.unifiprojects.app.appichetto.repositories;
 
 import java.util.List;
 
+import com.unifiprojects.app.appichetto.exceptions.AlreadyExistentException;
 import com.unifiprojects.app.appichetto.models.User;
 
 public interface UserRepository {
-	public void save(User user);
+	public void save(User user) throws IllegalArgumentException, AlreadyExistentException;
 
 	public User findById(Long id);
 
