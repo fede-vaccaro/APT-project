40647120547ab9c7f94170cd6b12b84309f72221
fede-vaccaro <federico.vaccaro@live.it>
2019-09-28 12:12:25 +0200

    feat: added configuration for integration tests

---
+++ com.unifiprojects.app.appichetto/src/it/java/com/unifiprojects/app/appichetto/repositories/UserRepositoryHibernateIT.java
@@ -0,0 +1,14 @@
+package com.unifiprojects.app.appichetto.repositories;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserRepositoryHibernateIT {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
