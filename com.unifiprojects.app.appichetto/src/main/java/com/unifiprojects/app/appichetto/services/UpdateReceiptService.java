c7f429b7d12d0c91cb2421986a1509d222dba9a3
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-10-14 21:18:10 +0200

    feat: implementing execute method and related test

--- com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/services/UpdateReceiptService.java
+++ com.unifiprojects.app.appichetto/src/main/java/com/unifiprojects/app/appichetto/services/UpdateReceiptService.java
@@ -2,17 +2,46 @@ package com.unifiprojects.app.appichetto.services;
 
 import com.unifiprojects.app.appichetto.controllers.ReceiptController;
 import com.unifiprojects.app.appichetto.managers.ReceiptManager;
-import com.unifiprojects.app.appichetto.views.ReceiptView;
+import com.unifiprojects.app.appichetto.models.Receipt;
+import com.unifiprojects.app.appichetto.repositories.ReceiptRepository;
+import com.unifiprojects.app.appichetto.repositories.UserRepository;
+import com.unifiprojects.app.appichetto.swingviews.ReceiptSwingView;
 
-public class UpdateReceiptService implements AppIchettoService{
+public class UpdateReceiptService implements AppIchettoService {
 
 	private ReceiptController receiptController;
 	private ReceiptManager receiptManager;
-	private ReceiptView receiptView;
-	
-	@Override
-	public void start(Object object) {
-		
+	private ReceiptSwingView receiptView;//TODO ReceiptSwingView in a concrete class because some methods are in ObservableFrame 
+	private Receipt receipt;
+
+	public UpdateReceiptService(ReceiptRepository receiptRepository, UserRepository userRepository) {
+		receiptManager = new ReceiptManager(receiptRepository);
+		receiptView = new ReceiptSwingView();
+		receiptController = new ReceiptController(receiptView, userRepository);
+		receiptView.setReceiptController(receiptController);
+		receiptView.setUsers();
 	}
 
+	@Override
+	public void execute(Object object) {
+		if (!(object instanceof Receipt))
+			throw new IllegalArgumentException("Argument must be Receipt");
+		receipt = (Receipt) object;
+		receiptManager.uploadReceipt(receipt);
+		receiptController.uploadReceiptManager(receiptManager);
+		receiptView.show();
+	}
+
+	void setReceiptManager(ReceiptManager receiptManager) {
+		this.receiptManager = receiptManager;
+	}
+
+	void setReceiptView(ReceiptSwingView receiptView) {
+		this.receiptView = receiptView;
+	}
+
+	void setReceiptController(ReceiptController receiptController) {
+		this.receiptController = receiptController;
+	}
+	
 }
---
+++ com.unifiprojects.app.appichetto/src/test/java/com/unifiprojects/app/appichetto/services/UpdateReceiptServiceTest.java
@@ -0,0 +1,98 @@
+package com.unifiprojects.app.appichetto.services;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.verify;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+
+import com.unifiprojects.app.appichetto.controllers.ReceiptController;
+import com.unifiprojects.app.appichetto.managers.ReceiptManager;
+import com.unifiprojects.app.appichetto.models.Receipt;
+import com.unifiprojects.app.appichetto.repositories.ReceiptRepository;
+import com.unifiprojects.app.appichetto.repositories.UserRepository;
+import com.unifiprojects.app.appichetto.swingviews.ReceiptSwingView;
+
+public class UpdateReceiptServiceTest {
+	@Mock
+	private ReceiptRepository receiptRepository;
+	
+	@Mock
+	private UserRepository userRepository;
+	
+	@Spy
+	private ReceiptManager receiptManager = new ReceiptManager(receiptRepository);
+	
+	@Spy
+	private ReceiptSwingView receiptView = new ReceiptSwingView();
+
+	@Spy
+	private ReceiptController receiptController = new ReceiptController(receiptManager, receiptView, userRepository);
+	
+	
+	@InjectMocks
+	private UpdateReceiptService updateReceiptService;
+
+	@Before
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void testWhenArgumentOfExecuteIsNotAReceiptThenExceptionIsThrown() {
+		try {
+			updateReceiptService.execute(new Object());
+			fail("Wrong argument");
+		} catch (IllegalArgumentException e) {
+			assertThat(e.getMessage()).isEqualTo("Argument must be Receipt");
+		}
+	}
+
+	@Test
+	public void testExcecuteInitializeUpdateReceiptContext() {
+		updateReceiptService.execute(new Receipt());
+		
+		assertThat(receiptView).isNotNull();
+		assertThat(receiptController).isNotNull();
+		assertThat(receiptManager).isNotNull();
+	}
+
+	@Test
+	public void testExcecuteCallUploadReceiptInReceiptManager() {
+		Receipt receipt = spy(new Receipt());
+		updateReceiptService.setReceiptManager(receiptManager);
+		
+		updateReceiptService.execute(receipt);
+
+		verify(receiptManager).uploadReceipt(receipt);
+	}
+
+	@Test
+	public void testExcecuteCallUploadReceiptManagerInReceiptController() {
+		Receipt receipt = spy(new Receipt());
+		updateReceiptService.setReceiptManager(receiptManager);
+		updateReceiptService.setReceiptController(receiptController);
+		
+		updateReceiptService.execute(receipt);
+		
+		verify(receiptController).uploadReceiptManager(receiptManager);
+	}
+
+	@Test
+	public void testExcecuteCallShowReceiptView() {
+		Receipt receipt = new Receipt();
+		updateReceiptService.setReceiptView(receiptView);
+		
+		updateReceiptService.execute(receipt);
+		
+		verify(receiptView).show();
+	}
+
+}
