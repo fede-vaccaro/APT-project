d8a4ceb7aabff0b21bc0ba1582cb0abac7d4abd9
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2019-12-01 16:02:05 +0100

    feat: added new scenario

--- appichetto-E2E/src/bdd/java/com/unifiprojects/app/appichetto/bdd/steps/AppichettoSteps.java
+++ appichetto-E2E/src/bdd/java/com/unifiprojects/app/appichetto/bdd/steps/AppichettoSteps.java
@@ -3,6 +3,7 @@ package com.unifiprojects.app.appichetto.bdd.steps;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.swing.launcher.ApplicationLauncher.application;
 
+import javax.persistence.EntityManager;
 import javax.swing.JFrame;
 
 import org.assertj.swing.core.BasicRobot;
@@ -12,6 +13,14 @@ import org.assertj.swing.core.matcher.JTextComponentMatcher;
 import org.assertj.swing.finder.WindowFinder;
 import org.assertj.swing.fixture.FrameFixture;
 
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import com.unifiprojects.app.appichetto.models.User;
+import com.unifiprojects.app.appichetto.modules.EntityManagerModule;
+import com.unifiprojects.app.appichetto.modules.RepositoriesModule;
+import com.unifiprojects.app.appichetto.repositories.UserRepositoryHibernate;
+
+import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -19,15 +28,40 @@ import io.cucumber.java.en.When;
 
 public class AppichettoSteps {
 
+	private UserRepositoryHibernate userRepository;
+	private EntityManager entityManager;
 	private FrameFixture window;
 
+	public void wipeTablesBeforeTest() {
+		entityManager.getTransaction().begin();
+		entityManager.createNativeQuery("do\n" + "$$\n" + "declare\n" + "  l_stmt text;\n" + "begin\n"
+				+ "  select 'truncate ' || string_agg(format('%I.%I', schemaname, tablename), ',')\n"
+				+ "    into l_stmt\n" + "  from pg_tables\n" + "  where schemaname in ('public');\n" + "\n"
+				+ "  execute l_stmt;\n" + "end;\n" + "$$").executeUpdate();
+		entityManager.getTransaction().commit();
+		entityManager.clear();
+	}
+	
 	@Before
 	public void setUp() {
-
+		Injector entityManagerInjector = Guice.createInjector(new EntityManagerModule());
+		Injector repositoryInjector = entityManagerInjector.createChildInjector(new RepositoriesModule());
+		entityManager = entityManagerInjector.getInstance(EntityManager.class);
+		userRepository = repositoryInjector.getInstance(UserRepositoryHibernate.class);
+	}
+	
+	@After
+	public void cleanAndTearDown() {
+		wipeTablesBeforeTest();
+//		window.close();
+		window.cleanUp();
 	}
 
-	@Given("The database of {string} not contain {string} user")
-	public void the_database_of_not_contain_user(String string, String string2) {
+	@Given("The database contains user {string} with {string} password")
+	public void the_database_contains_user_with_password(String string, String string2) {
+		entityManager.getTransaction().begin();
+		userRepository.save(new User(string, string2));
+		entityManager.getTransaction().commit();
 	}
 
 	@When("{string} view shows")
@@ -56,6 +90,10 @@ public class AppichettoSteps {
 	@Then("The view contain the following message {string}")
 	public void the_view_contain_the_following_message(String string) {
 		assertThat(window.label("errorMsg").text()).contains(string);
+	}
+	
+	@Then("{string} view disappear and {string} view shows")
+	public void view_disappear_and_view_shows(String string, String string2) {
 	}
 }
 
--- appichetto-E2E/src/bdd/resources/apphichetto.feature
+++ appichetto-E2E/src/bdd/resources/apphichetto.feature
@@ -2,11 +2,16 @@ Feature: Appichetto Application Frame
   Specifications of the behavior of Appichetto
 
   Scenario: Log in error because not signed
-  	
-  	When "Login View" view shows
+    When "Login" view shows
     And Write "Giuseppe" in "Username" text box
     And Write "Gpsw" in "Password" text box
     And Click "Log-in" button
     Then The view contain the following message "User not signed in yet"
 
- 
+  Scenario: Log in success
+    Given The database contains user "Giuseppe" with "Gpsw" password
+    When "Login" view shows
+    And Write "Giuseppe" in "Username" text box
+    And Write "Gpsw" in "Password" text box
+    And Click "Log-in" button
+    Then "Login" view disappear and "Homepage" view shows
