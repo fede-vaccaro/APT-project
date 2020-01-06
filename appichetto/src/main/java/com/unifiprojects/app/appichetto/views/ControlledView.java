fcfb5045a8124c4edd7bcdfd375aa47fa602bd6c
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2020-01-06 12:47:05 +0100

    chore: deleted unuseful classes

--- appichetto/src/main/java/com/unifiprojects/app/appichetto/views/ControlledView.java
+++
@@ -1,9 +0,0 @@
-package com.unifiprojects.app.appichetto.views;
-
-import com.unifiprojects.app.appichetto.controllers.UserController;
-
-public interface ControlledView {
-	public abstract UserController getController();
-
-	public abstract void updateData();
-}
