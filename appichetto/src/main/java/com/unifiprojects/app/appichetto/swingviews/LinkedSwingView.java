6707828a9e922ff71cb736c3a7b444feda4fadbc
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2020-01-06 12:46:26 +0100

    feat: clean and implemented only IView methods

--- appichetto/src/main/java/com/unifiprojects/app/appichetto/swingviews/LinkedSwingView.java
+++ appichetto/src/main/java/com/unifiprojects/app/appichetto/swingviews/LinkedSwingView.java
@@ -1,43 +1,19 @@
 package com.unifiprojects.app.appichetto.swingviews;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 
-public abstract class LinkedSwingView{
+public abstract class LinkedSwingView implements IView{
 
-	protected JButton btnHome;
 	protected JFrame frame;
-	protected LinkedSwingView previousLinkedSwingView;
-	
-	public void setLinkedSwingView(LinkedSwingView linkedSwingView) {
-		this.previousLinkedSwingView = linkedSwingView;
-	}
-
-	public LinkedSwingView getLinkedSwingView() {
-		return previousLinkedSwingView;
-	}
-	
-	public LinkedSwingView() {
-		btnHome = new JButton("Back");
-		btnHome.setName("homeBtn");
-		btnHome.addActionListener(e -> goBack());
-	}
-
+		
 	public JFrame getFrame() {
 		return frame;
 	}
-
-	public JButton getBtnHome() {
-		return btnHome;
-	}
 	
-	public void goBack() {
-		previousLinkedSwingView.show();
-		getFrame().dispose();
-	}
-
+	@Override
 	public void show() {
 		getFrame().setVisible(true);
 	}
+
 	
 }
