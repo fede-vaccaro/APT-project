e832c1e773bef28611f30868f2f8d8e7fe9638a0
Giuseppe Palazzolo <palazzolo1995@gmail.com>
2020-01-14 18:51:58 +0100

    feat: injected receiptswingview from showhistorymodule

--- appichetto-guice/src/main/java/com/unifiprojects/app/appichetto/modules/ShowHistoryModule.java
+++ appichetto-guice/src/main/java/com/unifiprojects/app/appichetto/modules/ShowHistoryModule.java
@@ -1,10 +1,13 @@
 package com.unifiprojects.app.appichetto.modules;
 
 import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
 import com.google.inject.Provides;
 import com.google.inject.assistedinject.FactoryModuleBuilder;
 import com.unifiprojects.app.appichetto.controllers.ShowHistoryController;
 import com.unifiprojects.app.appichetto.factories.ShowHistoryControllerFactory;
+import com.unifiprojects.app.appichetto.swingviews.ReceiptSwingView;
 import com.unifiprojects.app.appichetto.swingviews.ShowHistoryViewSwing;
 import com.unifiprojects.app.appichetto.views.ShowHistoryView;
 
@@ -18,8 +21,14 @@ public class ShowHistoryModule extends AbstractModule {
 
 	@Provides
 	public ShowHistoryViewSwing view(ShowHistoryControllerFactory controllerFactory) {
+		Injector persistenceInjector = Guice.createInjector(new EntityManagerModule());
+
+		Injector injector = persistenceInjector.createChildInjector(new RepositoriesModule(),
+				new ReceiptModule(), new UserPanelModule());
+		
 		ShowHistoryViewSwing view = new ShowHistoryViewSwing();
 		view.setShowHistoryController(controllerFactory.create(view));
+		view.setReceiptView(injector.getInstance(ReceiptSwingView.class));
 		return view;
 	}
 
